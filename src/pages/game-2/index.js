4fe957b8af352edcfba269b85c50a3bc344f313a
PK <prakrutipk@northwesternmutual.com>
2022-10-21 11:00:15 -0400

    fix padding; add SVG, audio, gradient

--- src/pages/game-2/index.js
+++ src/pages/game-2/index.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 import './game-2.css';
+import { ReactComponent as BloodCell } from './blood-cell.svg';
+import useAudio from '../../hooks/use-audio';
 
 const questions = require('./questions.json');
 
-export default function Game2() {
+export default function SickleCellQuiz() {
   const [showFinalResults, setFinalResults] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const SHORT_YAY_URL = `${process.env.PUBLIC_URL}/short-yay.mp3`;
+  const AWW_URL = `${process.env.PUBLIC_URL}/aww.mp3`;
+  const FUNNY_YAY_URL = `${process.env.PUBLIC_URL}/funny-yay.mp3`;
+  const [playingYay, toggleYay] = useAudio(SHORT_YAY_URL);
+  const [playingAww, toggleAww] = useAudio(AWW_URL);
+  const [playingFunny, toggleFunny] = useAudio(FUNNY_YAY_URL);
+
   const choiceClicked = (isCorrect) => {
     if (isCorrect) {
       setScore(score + 1);
-      // TODO: turn correct choice green if correct?
+      toggleYay();
+    } else {
+      toggleAww();
     }
+
     if (currentQuestion + 1 < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
     } else {
       setFinalResults(true);
+      toggleFunny();
     }
   };
 
@@ -27,17 +40,20 @@ export default function Game2() {
 
   return (
     <div className="App">
-      <h1>Sickle Cell Anemia Quiz</h1>
+      <h1>Sickle Cell Disease Quiz</h1>
       <h2>
         Current Score:
         {' '}
         {score}
       </h2>
+      <div className="blood-cell-container">
+        <BloodCell />
+      </div>
 
       { showFinalResults
         ? (
           <div className="final-results">
-            <h1> Final Results</h1>
+            <h1> Final Results: </h1>
             <h2>
               {score}
               {' '}
