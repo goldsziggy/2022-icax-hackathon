cd3da2a2d24548d04e82109a361cced21fb5481b
Matthew Zygowicz <matthewzygowicz@northwesternmutual.com>
2022-11-01 15:48:52 -0500

    feat: titlebar svg, notification - added files and tweaked notification pause

--- src/App.js
+++ src/App.js
@@ -44,8 +44,6 @@ function gameLoop(pollFunctions) {
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
         progress: undefined,
         theme: "light",
       });
